5c36bca22ef8a343bfaa96e4cbe70e13672f65f7
Dobos Cosmin <cosmin.dobos@graffino.com>
2023-11-23 17:20:05 +0200

    add scripts and styles (not final version)

--- graffino-elementor-extra.php
+++ graffino-elementor-extra.php
@@ -16,14 +16,7 @@ if (! defined('ABSPATH') ) {
     exit; // Exit if accessed directly.
 }
 
-
-
 function graffino_elementor_extra() {
-
-
 	require_once( __DIR__ . '/includes/plugin.php' );
 }
 add_action( 'plugins_loaded', 'graffino_elementor_extra' );
-
-
-
